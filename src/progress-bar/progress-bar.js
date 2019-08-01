e8ad6faa9317362114c65bdec8d894b852818896
Lucas Credie <lucascredie@gmail.com>
2019-08-01 11:54:26 -0700

    init

---
+++ src/progress-bar/progress-bar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+/*
+    Shape: circular, linear, custom
+    Orientation: from left to right ot right to left
+    Type: Percentage Bar or a Unit Bar
+    Progress: percentage/units
+    Total: max percentage/units
+    Animated = true || false
+        AnimateFrom: percentage/units
+        AnimationDuration: 200ms
+ */
+
+class ProgressBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+    
+}
+
+export default ProgressBar;
